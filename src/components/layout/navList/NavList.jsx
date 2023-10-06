be1a0605cfea8c4af54564eb8311b32fe35b92d9
HypeBeast18 <dimadeg2202@gmail.com>
2023-10-06 21:59:06 +0500

    More

--- src/components/layout/navList/NavList.jsx
+++ src/components/layout/navList/NavList.jsx
@@ -5,16 +5,16 @@ import NavItem from '../../ui/navItem/NavItem'
 
 import {list} from '../../../data/List.data'
 
-const NavList = () => {
 
-  
+
+const NavList = () => {
 
 
   return (
     <div className='bg-zinc-900 h-screen'>
       <nav className='text-left '>
       {list.map(l => (
-        <NavItem key={l.path} name={l.name} numbers={useSelector(state => state.todos.todos).length} />
+        <NavItem key={l.path} listItem={l} />
       ))}
       </nav>
     </div>
--- src/components/ui/navItem/NavItem.jsx
+++ src/components/ui/navItem/NavItem.jsx
@@ -3,14 +3,15 @@ import { useNavigate } from "react-router-dom"
 
 
 
-const NavItem = ({name, path, numbers }) => {
+const NavItem = ({listItem}) => {
 
   const navigate = useNavigate()
 
+  const reduxName = `useSelector(state => state.${listItem.reduxName}.${listItem.reduxName})` 
 
 
   return (
-    <button className='block'>{name} {numbers} </button>
+    <button className='block'>{listItem.name} {reduxName} </button>
   )
 }
 
